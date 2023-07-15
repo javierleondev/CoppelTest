20f462555029be9e1a00e1951295ca58d0088753
leoalock <61290744+javierleondev@users.noreply.github.com>
2023-07-14 18:50:51 -0700

    DB files
    Added jdbc connector dependency and created a class to connect to the database

---
+++ src/main/java/com/mycompany/coppel/db/DatabaseConnector.java
@@ -0,0 +1,28 @@
+package com.mycompany.coppel.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Pc
+ */
+public class DatabaseConnector {
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/perro";
+    
+    static final String USER = "root";
+    static final String PASS = "";
+    
+    public Connection connectToDatabase(){
+        Connection conn = null;
+        try{
+            Class.forName(JDBC_DRIVER);
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        }catch(SQLException | ClassNotFoundException se){
+            System.out.print(se);
+        }
+        return conn;
+    }
+}
